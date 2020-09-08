41fdedd97db2875d49671c802c08bd61e5da31df
Adelia <adelgants@gmail.com>
2020-09-09 01:48:43 +0300

    add style

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,76 @@ export default {
 
 <style lang="scss">
 
+$input-width: 180px;
+$input-padding: 5px 5px;
+$input-height: 15px;
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10vh 0;
+  background-color: #e2e2ee;
+}
+
+.create {
+  margin: auto;
+  background-color: #ffffff;
+  padding: 30px;
+}
+
+.btn {
+  width: 150px;
+  margin: 0 30px 30px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.form {
+  display: flex;
+  flex-wrap: wrap;
+  &-block {
+    padding: 30px;
+  }
+
+  &-group {
+    margin-bottom: 15px;
+    &__label {
+      display: block;
+      margin-bottom: 10px;
+      
+      &_radio {
+        margin-left: 5px;
+        margin-right: 10px;
+      }
+
+      &_checkbox {
+        margin-left: 5px;
+      }
+    }
+
+    &__input {
+      width: $input-width;
+      height: $input-height;
+      padding: $input-padding;
+
+      &_textarea {
+        padding: $input-padding;
+        width: $input-width;
+        height: 100px;
+        resize: none;
+      }
+
+      &_select {
+        width: $input-width;
+        padding: $input-padding;
+      }
+    }
+  }
+}
 </style>
--- src/components/clients.vue
+++ src/components/clients.vue
@@ -1,8 +1,7 @@
 <template>
   <form class="create">
-
+  <div class='form'>
     <div class="form-block">
-    
       <div class="form-group">
         <label for="surname" class="form-group__label">Фамилия:</label>
         <input id="surname" class="form-group__input" type="text">
@@ -19,26 +18,26 @@
       </div> 
 
       <div class="form-group">
-        <label for="bdate" class="form-group__label">Дата рождения</label>
+        <label for="bdate" class="form-group__label">Дата рождения:</label>
         <input id="bdate" class="form-group__input" type="date">
       </div>
       
       <div class="form-group">
-        <label for="tel" class="form-group__label">Номер телефона</label>
+        <label for="tel" class="form-group__label">Номер телефона:</label>
         <input id="tel" class="form-group__input" inputmode="tel">
       </div> 
 
       <div class="form-group">
-        <input name='sex' id='male' class="form-group__input" type="radio">
-        <label for="male" class="form-group__label" checked>Мужчина</label>
+        <input name='sex' id='male' class="form-group__input_radio" type="radio">
+        <label for="male" class="form-group__label_radio" checked>Мужчина:</label>
         
-        <input name='sex' id='female' class="form-group__input" type="radio">
-        <label for="female" class="form-group__label">Женщина</label>
+        <input name='sex' id='female' class="form-group__input_radio" type="radio">
+        <label for="female" class="form-group__label_radio">Женщина:</label>
       </div>
       
       <div class="form-group">
-        <label for="clients" class="form-group__label">Группа клиентов</label>
-        <select id="clients" class="form-group__mselect" multiple size="3">
+        <label for="clients" class="form-group__label">Группа клиентов:</label>
+        <select id="clients" class="form-group__input_select" multiple size="3">
           <option>VIP</option>
           <option>Проблемные</option>
           <option>ОМС</option>
@@ -46,8 +45,8 @@
       </div>
 
       <div class="form-group">
-        <label for="doctor" class="form-group__label">Группа клиентов</label>
-        <select id="doctor" class="form-group__select" size="1">
+        <label for="doctor" class="form-group__label">Лечащий врач:</label>
+        <select id="doctor" class="form-group__input_select" size="1">
           <option>Иванов</option>
           <option>Захаров</option>
           <option>Чернышева</option>
@@ -55,47 +54,47 @@
       </div>
 
       <div class="form-group">
-        <input id='sms' class="form-group__input" type="checkbox">
-        <label for="sms" class="form-group__label">Не отправлять СМС</label>
+        <input id='sms' class="form-group__input__checkbox" type="checkbox">
+        <label for="sms" class="form-group__label_checkbox">Не отправлять СМС</label>
       </div>
     </div>
 
     <div class="form-block">
       <div class="form-group">
-        <label for="index" class="form-group__label">Индекс</label>
+        <label for="index" class="form-group__label">Индекс:</label>
         <input id="index" class="form-group__input" inputmode="numeric">
       </div> 
 
       <div class="form-group">
-        <label for="country" class="form-group__label">Страна</label>
+        <label for="country" class="form-group__label">Страна:</label>
         <input id="country" class="form-group__input">
       </div> 
 
       <div class="form-group">
-        <label for="region" class="form-group__label">Область</label>
+        <label for="region" class="form-group__label">Область:</label>
         <input id="region" class="form-group__input">
       </div> 
 
       <div class="form-group">
-        <label for="city" class="form-group__label">Город</label>
+        <label for="city" class="form-group__label">Город:</label>
         <input id="city" class="form-group__input">
       </div> 
 
       <div class="form-group">
-        <label for="street" class="form-group__label">Улица</label>
-        <input id="street" class="form-group__input">
+        <label for="street" class="form-group__label">Улица:</label>
+        <textarea id="street" class="form-group__input_textarea"></textarea>
       </div> 
 
       <div class="form-group">
-        <label for="home" class="form-group__label">Дом</label>
+        <label for="home" class="form-group__label">Дом:</label>
         <input id="home" class="form-group__input">
       </div> 
     </div>
 
     <div class="form-block">
       <div class="form-group">
-        <label for="document" class="form-group__label">Тип документа</label>
-        <select id="document" class="form-group__select" size="1">
+        <label for="document" class="form-group__label">Тип документа:</label>
+        <select id="document" class="form-group__input_select" size="1">
           <option>Паспорт</option>
           <option>Свидетельство о рождении</option>
           <option>Вод. удостоверение</option>
@@ -103,27 +102,27 @@
       </div>
 
       <div class="form-group">
-        <label for="series" class="form-group__label">Серия</label>
+        <label for="series" class="form-group__label">Серия:</label>
         <input id="series" class="form-group__input" inputmode="numeric">
       </div>
 
       <div class="form-group">
-        <label for="number" class="form-group__label">Номер</label>
+        <label for="number" class="form-group__label">Номер:</label>
         <input id="nubmer" class="form-group__input" inputmode="numeric">
       </div>
 
 
       <div class="form-group">
-        <label for="issued" class="form-group__label">Кем выдан</label>
-        <textarea id="issued" class="form-group__input"></textarea>
+        <label for="issued" class="form-group__label">Кем выдан:</label>
+        <textarea id="issued" class="form-group__input_textarea"></textarea>
       </div> 
 
       <div class="form-group">
-        <label for="pass-date" class="form-group__label">Дата выдачи</label>
+        <label for="pass-date" class="form-group__label">Дата выдачи:</label>
         <input id="pass-date" class="form-group__input" type='date'>
       </div> 
     </div>
-
+  </div>
     <button type="submit" class="btn">Создать</button>
   </form>
 </template>
